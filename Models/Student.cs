4b315e4f47d037635a4864c0cc6eaba41a011716
Lenora Stevens <114180320+lenorastevens@users.noreply.github.com>
2024-03-22 16:35:20 -0500

    Add project files.

---
+++ Models/Student.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SWAUniversity.Models
+{
+    public class Student : Person
+    {        
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true), Display(Name = "Enrollement Date")]
+        public DateTime EnrollmentDate { get; set; }
+
+        public ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
